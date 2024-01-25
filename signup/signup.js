14b6c98cf6a2748bb7162de4fced051391ea9a5f
Abrar Edel <153538009+Bureqha@users.noreply.github.com>
2024-01-25 18:07:42 +0300

    Add files via upload

---
+++ signup/signup.js
@@ -0,0 +1,7 @@
+document.getElementById('signupForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+  
+    // Add your form submission logic here (e.g., validation, AJAX request)
+    console.log('Form submitted!');
+  });
+  
